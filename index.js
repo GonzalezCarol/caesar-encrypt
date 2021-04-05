b9d570643e3c5cf0a47a7fbebd26b994ecf6a5b4
carol <carolina.gonzalez.alvarenga@gmail.com>
2021-04-04 23:37:54 -0300

    challenge concluded

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const getDataFromCaesarCrypt = async () => {
   try {
     return await axios.get(
       "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=5806d4f485755b689b5677d1a1b0304ee5c2e606"
+      // https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=e8a7f2f5e4fb9eb704ec9f6f62e553efe66216d2"
     );
   } catch (error) {
     console.error(error);
@@ -18,7 +19,28 @@ const countBreeds = async () => {
   const jsonDataEncript = JSON.stringify(dataCaesarCrypt.data);
 
   if (fs.existsSync("answer.json")) {
-    console.log("File exist");
+    const answer = JSON.stringify(require("./answer.json"));
+
+    axios({
+      method: "post",
+      url:
+        "https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=5806d4f485755b689b5677d1a1b0304ee5c2e606",
+      // "https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=e8a7f2f5e4fb9eb704ec9f6f62e553efe66216d2",
+      data: answer,
+      file: "answer",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "multipart/form-data",
+      },
+    })
+      .then(function (response) {
+        //handle success
+        console.log(response);
+      })
+      .catch(function (response) {
+        //handle error
+        console.log(response);
+      });
   } else {
     fs.writeFile("answer.json", jsonDataEncript, function (err) {
       if (err) throw err;
